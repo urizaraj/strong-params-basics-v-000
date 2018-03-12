efa1a1b0b106ed3d74581ecb51096f47f50c2094
Alexander Urizar <urizaraj@gmail.com>
2018-03-12 18:55:09 +0000

    automatically backed up by learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -20,14 +20,17 @@ class PostsController < ApplicationController
 	  @post.save
 	  redirect_to post_path(@post)
 	end
-	 
+
 	def update
 	  @post = Post.find(params[:id])
 	  @post.update(post_params(:title))
 	  redirect_to post_path(@post)
 	end
-	 
-	private
 
+	private
+	
+	def post_params(*args)
+	  params.require(:post).permit(*args)
+	end
 
 end
